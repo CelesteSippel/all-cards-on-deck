f9902bc1cff87b021fa884091659b0912df0f496
Celeste Sippel <celestechristine@gmail.com>
2019-11-07 13:31:50 -0500

    adding for blackjack

--- public/main.js
+++ public/main.js
@@ -14,32 +14,58 @@ const ranks = [
   'Queen',
   'King'
 ]
+const values = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 const deck = []
+const playerHand = []
+const getCardValue = rank => {
+  if (rank === 'Ace') {
+    return 11
+  } else if (rank === 'King' || rank === 'Queen' || rank === 'Jack') {
+    return 10
+  } else {
+    return parseInt(rank)
+  }
+}
 
 const makeDeck = () => {
-  for (let suitsIndex = 0; suitsIndex < suits.length; suitsIndex++) {
-    for (let ranksIndex = 0; ranksIndex < ranks.length; ranksIndex++) {
-      deck.push(ranks[ranksIndex] + ' of ' + suits[suitsIndex])
+  for (let i = 0; i < suits.length; i++) {
+    for (let j = 0; j < ranks.length; j++) {
+      const card = {
+        rank: ranks[j],
+        suit: suits[i],
+        value: getCardValue(ranks[j])
+      }
+      deck.push(card)
     }
   }
 }
 const shuffleDeck = () => {
   for (let i = deck.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * i)
-    const dog = deck[i]
+    const temp = deck[i]
     deck[i] = deck[j]
-    deck[j] = dog
+    deck[j] = temp
   }
   console.log(shuffleDeck)
 }
 
-const dealCard = () => {
-  // document.querySelector('p').textContent = deck[0]
-  document.querySelector('p').textContent = deck.splice(deck.length - 1, 1)[0]
+const dealACard = () => {
+  const drawnCard = deck.pop()
+  playerHand.push(drawnCard)
+  const cardLi = document.createElement('li')
+  cardLi.textContent = drawnCard.rank + 'of' + drawnCard.suit
+  document.querySelector('.player-hand').appendChild(cardLi)
+
+  let sum = 0
+  for (let i = 0; i < playerHand.length; i++) {
+    sum += playerHand[i].value
+  }
+  document.querySelector('.player-sum').textContent = sum
 }
 const makeAndShuffleDeck = () => {
   makeDeck()
   shuffleDeck()
 }
-document.addEventListener('click', dealCard)
+
 document.addEventListener('DOMContentLoaded', makeAndShuffleDeck)
+document.querySelector('button').addEventListener('click', dealACard)
